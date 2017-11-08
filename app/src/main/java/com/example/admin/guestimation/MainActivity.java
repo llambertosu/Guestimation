be2748a026cde5104a2446d0e5b7844811c7daf0
msrusse <30694747+msrusse@users.noreply.github.com>
2017-11-08 08:45:35 -0600

    Added jtds driver for SQL connection, as well as adding connection variables to MainActivity.java. Also

--- app/src/main/java/com/example/admin/guestimation/MainActivity.java
+++ app/src/main/java/com/example/admin/guestimation/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.admin.guestimation;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaring layout button, edit texts
+    public EditText answer;
+    public Button submit;
+    public TextView question;
+
+    //Declaring connection variables
+    public Connection con;
+    String un,pass,db,ip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get values from the button, ExitText, and TextView
+        answer = (EditText) findViewById(R.id.answerBox);
+        submit = (Button) findViewById(R.id.submitButton);
+        question = (TextView) findViewById(R.id.questionView);
+
+        //Declare Server ip, userrname, database name, and password
+        ip = "guestimation.database.windows.net:1433";
+        db = "guestimation";
+        un = "user";
+        pass = "Cowboys2017";
+
     }
+
 }
