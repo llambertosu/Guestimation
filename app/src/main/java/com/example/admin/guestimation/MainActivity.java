72a091dcd9a08277b704735647da75e11b0e6b1b
msrusse <30694747+msrusse@users.noreply.github.com>
2017-11-13 23:15:44 -0600

    Adds <uses-permission> lines to AndroidManifest in order to access the internet from the device, as well as adding functionality to pull a single question from the database in the MainActivity.

--- app/src/main/java/com/example/admin/guestimation/MainActivity.java
+++ app/src/main/java/com/example/admin/guestimation/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.admin.guestimation;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +26,12 @@ public class MainActivity extends AppCompatActivity {
     public EditText answer;
     public Button submit;
     public TextView question;
+    public ProgressBar progressBar;
+    public Button getQuestion;
 
     //Declaring connection variables
     public Connection con;
-    String un,pass,db,ip;
+    String un, pass, db, ip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +42,107 @@ public class MainActivity extends AppCompatActivity {
         answer = (EditText) findViewById(R.id.answerBox);
         submit = (Button) findViewById(R.id.submitButton);
         question = (TextView) findViewById(R.id.questionView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        getQuestion = (Button) findViewById(R.id.getQuestion);
 
-        //Declare Server ip, userrname, database name, and password
+        //Declare Server ip, username, database name, and password
         ip = "guestimation.database.windows.net:1433";
         db = "guestimation";
         un = "user";
         pass = "Cowboys2017";
 
+        getQuestion.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                CheckQuestion checkSubmit = new CheckQuestion();
+                checkSubmit.execute("");
+            }
+        });
     }
+
+    public class CheckQuestion extends AsyncTask<String,String,String> {
+        String z = "";
+        Boolean isSuccess = false;
+        String name1 = "";
+
+        protected void onPreExecute() {
+            question.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String r) {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, r, Toast.LENGTH_LONG).show();
+            if (isSuccess) {
+                question = (TextView) findViewById(R.id.questionView);
+                question.setText(name1);
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                con = connectionclass();
+                if (con == null) {
+                    z = "Check your internet access and try again!";
+                }
+                else
+                    {
+                    String query = "select Question from Card where DeckID=1 and CardID=15"; //need to change the DeckID and CardID to be Session Variables
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery(query);
+                    if (rs.next())
+                    {
+                        name1 = rs.getString("Question");
+                        z = name1;
+                        isSuccess = true;
+                        con.close();
+                    } else {
+                        z = "Error retrieving question";
+                        isSuccess = false;
+                    }
+                }
+            } catch (Exception ex) {
+                isSuccess = false;
+                z = ex.getMessage();
+
+                Log.d("sql error", z);
+            }
+            return z;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    public Connection connectionclass()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //your database connection string goes below
+            ConnectionURL = "jdbc:jtds:sqlserver://guestimation.database.windows.net:1433;DatabaseName=guestimation;user=user@guestimation;password=Cowboys2017;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
+    }
+
 
 }
