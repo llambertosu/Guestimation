11635ed21e25fef70a4e044089baa4355a36de11
llambertosu <lee.van.lambert@okstate.edu>
2017-11-27 21:21:57 -0600

    Worked on homepage, created rancome gamePass class.

--- app/src/main/java/com/example/admin/guestimation/Homepage.java
+++ app/src/main/java/com/example/admin/guestimation/Homepage.java
@@ -1,14 +1,9 @@
 package com.example.admin.guestimation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
-
-import java.security.SecureRandom;
-import java.util.Locale;
-import java.util.Objects;
-import java.util.Random;
+import android.view.View;
 
 /**
  * Created by admin on 11/1/17.
@@ -19,71 +14,16 @@ public class Homepage extends AppCompatActivity{
     //but it is the random number generator for the gamekey.
 
 
-    public Button hostButton;
 
-    public static class RandomString {
-
-        /**
-         * Generate a random string.
-         */
-        public String nextString() {
-            for (int idx = 0; idx < buf.length; ++idx)
-                buf[idx] = symbols[random.nextInt(symbols.length)];
-            return new String(buf);
-        }
-
-        public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        public static final String lower = upper.toLowerCase(Locale.ROOT);
-
-        public static final String digits = "0123456789";
-
-        public static final String alphanum = upper + lower + digits;
-
-        private final Random random;
-
-        private final char[] symbols;
-
-        private final char[] buf;
-
-        public RandomString(int length, Random random, String symbols) {
-            if (length < 1) throw new IllegalArgumentException();
-            if (symbols.length() < 2) throw new IllegalArgumentException();
-            this.random = Objects.requireNonNull(random);
-            this.symbols = symbols.toCharArray();
-            this.buf = new char[length];
-        }
-
-        /**
-         * Create an alphanumeric string generator.
-         */
-        public RandomString(int length, Random random) {
-            this(length, random, alphanum);
-        }
-
-        /**
-         * Create an alphanumeric strings from a secure generator.
-         */
-        public RandomString(int length) {
-            this(length, new SecureRandom());
-        }
-
-        /**
-         * Create session identifiers.
-         */
-        public RandomString() {
-            this(4);
-        }
-
-    }
-
-    RandomString gameKey = new RandomString();
 
     @Override
     public void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+    }
 
-        hostButton = (Button) findViewById(R.id.hostButton);
+    public void onHostButton(View v) {
+        Intent intent = new Intent(getApplicationContext(), HostGame.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/admin/guestimation/HostGame.java
@@ -0,0 +1,204 @@
+package com.example.admin.guestimation;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 11/27/17.
+ */
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Random;
+
+public class HostGame extends AppCompatActivity {
+
+    public EditText nickname;
+    public EditText gamePass;
+    public Button loginButton;
+
+    public Connection con;
+    String un, pass, db, ip;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_game);
+
+        //Get values from the button, ExitText, and TextView
+        nickname = (EditText) findViewById(R.id.enterNickname);
+        gamePass = (EditText) findViewById(R.id.enterPassword);
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        //Declare Server ip, username, database name, and password
+        ip = "guestimation.database.windows.net:1433";
+        db = "guestimation";
+        un = "user";
+        pass = "Cowboys2017";
+
+        /*Are we supposed to be INSERT'ing or pulling from
+        the db and comparing values? line 94.
+         */
+
+    }
+
+    public class CheckLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
+        String name1 ="";
+
+        @Override
+        protected void onPostExecute(String r) {
+            if (isSuccess)
+            {
+                Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                intent2.putExtra("username", nickname.getText().toString());
+                intent2.putExtra("gamePass", gamePass.getText().toString());
+                intent2.putExtra("nextCard", "0");
+                startActivity(intent2);
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params)
+        {
+            String userNickname = nickname.getText().toString();
+            String gamePassword = gamePass.getText().toString();
+            try
+            {
+                con = connectionclass();
+                if (con == null)
+                {
+                    z = "Check your internet access and try again!";
+                }
+                else
+                {
+                    String query = "insert into Player (Nickname, GameID, Score) values ('" + userNickname + "', '" + gamePassword + "', 0)";
+                    Statement stmt = con.createStatement();
+                    stmt.executeUpdate(query);
+                    isSuccess = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                isSuccess = false;
+                z = ex.getMessage();
+
+                Log.d("sql error", z);
+            }
+            return z;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    public Connection connectionclass()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //your database connection string goes below
+            ConnectionURL = "jdbc:jtds:sqlserver://guestimation.database.windows.net:1433;DatabaseName=guestimation;user=user@guestimation;password=Cowboys2017;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
+    }
+
+    public static class RandomString {
+
+        /**
+         * Generate a random string.
+         */
+        public String nextString() {
+            for (int idx = 0; idx < buf.length; ++idx)
+                buf[idx] = symbols[random.nextInt(symbols.length)];
+            return new String(buf);
+        }
+
+        public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public static final String lower = upper.toLowerCase(Locale.ROOT);
+
+        public static final String digits = "0123456789";
+
+        public static final String alphanum = upper + lower + digits;
+
+        private final Random random;
+
+        private final char[] symbols;
+
+        private final char[] buf;
+
+        public RandomString(int length, Random random, String symbols) {
+            if (length < 1) throw new IllegalArgumentException();
+            if (symbols.length() < 2) throw new IllegalArgumentException();
+            this.random = Objects.requireNonNull(random);
+            this.symbols = symbols.toCharArray();
+            this.buf = new char[length];
+        }
+
+        /**
+         * Create an alphanumeric string generator.
+         */
+        public RandomString(int length, Random random) {
+            this(length, random, alphanum);
+        }
+
+        /**
+         * Create an alphanumeric strings from a secure generator.
+         */
+        public RandomString(int length) {
+            this(length, new SecureRandom());
+        }
+
+        /**
+         * Create session identifiers.
+         */
+        public RandomString() {
+            this(4);
+        }
+
+    }
+
+    public void onGameKeyPressed (View v) {
+        RandomString gameKey = new RandomString();
+
+    }
+}
+
