5f3d8a83eeb4ff9aa2b446cd3c6e3d8ca96cdb21
msrusse <30694747+msrusse@users.noreply.github.com>
2017-12-13 19:52:06 -0600

    Probably fixes the score update query this time, maybe

--- app/src/main/java/com/example/admin/guestimation/ScoreActivity.java
+++ app/src/main/java/com/example/admin/guestimation/ScoreActivity.java
@@ -221,10 +221,18 @@ public class ScoreActivity extends AppCompatActivity {
             int counter = 0;
             while (counter < responses.size())
             {
-                if (answer - Integer.parseInt(responses.get(counter)) < answer - winner && answer - Integer.parseInt(responses.get(counter)) >= 0)
-                {
-                    winner = Integer.parseInt(responses.get(counter));
+                if(winner != 9999) {
+                    if (answer - Integer.parseInt(responses.get(counter)) < answer - winner && answer - Integer.parseInt(responses.get(counter)) >= 0) {
+                        winner = Integer.parseInt(responses.get(counter));
+                    }
                 }
+                else
+                {
+                    if (answer - Integer.parseInt(responses.get(counter)) < winner && answer - Integer.parseInt(responses.get(counter)) >= 0) {
+                        winner = Integer.parseInt(responses.get(counter));
+                    }
+                }
+
                 counter += 1;
             }
         }
